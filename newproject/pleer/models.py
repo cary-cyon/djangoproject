08d5d49fee4e7f34acb385a841a377670f2979fb
cary-cyon <kozvlad99@gmail.com>
2021-10-24 15:53:14 +0300

    Example Migration

--- newproject/pleer/models.py
+++ newproject/pleer/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+"""класс плейлиста """
+
+
+class PlayList(models.Model):
+    name = models.CharField("Playlist name", max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+""" класс композиции """
+
+
+class Composition(models.Model):
+    name = models.CharField("composition name", max_length=200)
+    author = models.CharField("author name", max_length=200)
+    playlist = models.ForeignKey(PlayList, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
