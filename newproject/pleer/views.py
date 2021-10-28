5b927831f37ac4f9232e4a8f4eb9fb6d17f3f135
cary-cyon <kozvlad99@gmail.com>
2021-10-28 14:45:08 +0300

    first template

--- newproject/pleer/views.py
+++ newproject/pleer/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Composition
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("FYDGASKGDAILSDIUASDYI")
+    all_composition = Composition.objects.all()
+    return render(request, 'list.html', {'composition': all_composition})
+
 
 def test(request):
     return HttpResponse("SDFGHJKL:JHGFDSASD")
