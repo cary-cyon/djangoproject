81f03d60e2dd888d073707eb6d1847e7d887c08e
cary-cyon <kozvlad99@gmail.com>
2021-10-29 17:09:34 +0300

    New migrate

--- newproject/pleer/models.py
+++ newproject/pleer/models.py
@@ -7,6 +7,7 @@ from django.db import models
 
 class PlayList(models.Model):
     name = models.CharField("Playlist name", max_length=200)
+    cover = models.ImageField(upload_to="images", null=True)
 
     def __str__(self):
         return self.name
@@ -19,6 +20,8 @@ class Composition(models.Model):
     name = models.CharField("composition name", max_length=200)
     author = models.CharField("author name", max_length=200)
     playlist = models.ForeignKey(PlayList, on_delete=models.CASCADE, null=True)
+    cover = models.ImageField(upload_to="images", null=True)
+    audio = models.FileField(upload_to="audios", null=True)
 
     def __str__(self):
         return self.name
