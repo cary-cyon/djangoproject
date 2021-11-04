50463f8ac304a745a89b61f564cd31ce5087a7a5
cary-cyon <kozvlad99@gmail.com>
2021-11-04 20:18:48 +0300

    somejscjde

---
+++ newproject/pleer/static/pleer/js/pleer_button.js
@@ -0,0 +1,11 @@
+let audio = document.getElementById("audio");    // Берём элемент audio
+let time = document.querySelector(".time");      // Берём аудио дорожку
+let btnPlay = document.querySelector(".play");   // Берём кнопку проигрывания
+let btnPause = document.querySelector(".pause"); // Берём кнопку паузы
+let btnPrev = document.querySelector(".prev");   // Берём кнопку переключения предыдущего трека
+let btnNext = document.querySelector(".next");   // Берём кнопку переключение следующего трека
+let bt = document.getElementById("pause");
+
+bt.addEventListener("click", function() {
+    bt.innerHTML = "efadsfas";
+});
