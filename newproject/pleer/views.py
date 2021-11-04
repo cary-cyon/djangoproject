618afa5371668db0f552f67a6939d1e0cdb4de91
cary-cyon <kozvlad99@gmail.com>
2021-11-04 16:36:08 +0300

    ref+media_root

--- newproject/pleer/views.py
+++ newproject/pleer/views.py
@@ -6,7 +6,7 @@ from .models import Composition
 
 def index(request):
     all_composition = Composition.objects.all()
-    return render(request, 'list.html', {'all_composition': all_composition})
+    return render(request, 'pleer/list.html', {'all_composition': all_composition})
 
 
 def test(request):
