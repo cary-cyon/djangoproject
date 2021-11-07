8f18adfbb9d8436ddab77dd43b8d97bb27abcef9
DanilaPalkin <danilapalkin@gmail.com>
2021-11-08 00:40:34 +0300

    Список плейлистов
    Попытка вывести плейлисты и принадлежащие им треки (доделать)

--- newproject/playlist/admin.py
+++ newproject/playlist/admin.py
@@ -1,1 +1,3 @@
+from django.contrib import admin
+from .models import PlayList
 
--- newproject/playlist/views.py
+++ newproject/playlist/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from pleer.models import Composition
+from pleer.models import Composition, PlayList
 
 
 def index(request):
     compositions = Composition.objects.all()
     return render(request, 'playlist/playlist.html', {'title': 'Список треков', 'compositions': compositions})
+
+
+def getPlaylists(request):
+    playlists = PlayList.objects.all()
+    compositions = Composition.objects.filter(PlayList.name == Composition.name)
+    return render(request, 'playlist/all_playlists.html', {'title': 'Список плейлистов', 'playlists': playlists, 'compositions': compositions})
